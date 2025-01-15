7cc3cd51412950d4cd43663823a42e53b0870157
Kilyong Jeong <uzoolove@gmail.com>
2025-01-15 17:09:42 +0900

    ♻️ 에러 로그만 전용 파일에 남기도록 추가

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,11 +1,26 @@
 import tracer from 'tracer';
+
 let logger;
+let errorLogger;
+
 if (process.env.NODE_ENV == 'production') {
   logger = tracer.dailyfile({
     root: 'logs',
     maxLogFiles: 365,
     allLogsFileName: 'log',
     level: 3,  // log(0), trace(1), debug(2), info(3), warn(4), error(5), fatal(6)
+    format: ['{{timestamp}} <{{title}}> {{file}}:{{line}}({{method}}()) {{message}}'],
+    dateformat: 'yyyy-mm-dd HH:MM:ss',
+    inspectOpt: {
+      depth: 5
+    }
+  });
+
+  errorLogger = tracer.dailyfile({
+    root: 'logs',
+    maxLogFiles: 365,
+    allLogsFileName: 'error',
+    level: 5,  // error(5), fatal(6)
     format: ['{{timestamp}} <{{title}}> {{file}}:{{line}}({{method}}()) {{message}}'],
     dateformat: 'yyyy-mm-dd HH:MM:ss',
     inspectOpt: {
@@ -21,6 +36,17 @@ if (process.env.NODE_ENV == 'production') {
       depth: 10
     }
   });
+
+  errorLogger = tracer.colorConsole({
+    level: 5,  // error(5), fatal(6)
+    format: ['{{timestamp}} <{{title}}> {{file}}:{{line}}({{method}}()) {{message}}'],
+    dateformat: 'yyyy-mm-dd HH:MM:ss',
+    inspectOpt: {
+      depth: 10
+    }
+  });
 }
+
+export { errorLogger };
 
 export default logger;
